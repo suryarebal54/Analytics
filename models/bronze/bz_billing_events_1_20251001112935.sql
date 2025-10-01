fc357c6a4edb8af9610c19e6c81f0194c896898a
suryarebal54 <suryarebal54@gmail.com>
2025-10-01 16:59:43 +0530

    Add initial DBT project files for raw to bronze transformation

---
+++ models/bronze/bz_billing_events_1_20251001112935.sql
@@ -0,0 +1,27 @@
+{{config(
+    materialized='table',
+    schema='bronze',
+    pre_hook="{% if this.identifier != 'bz_audit_log' %}
+                {% set start_time = modules.datetime.datetime.now() %}
+                {% do run_query('INSERT INTO bronze.bz_audit_log (source_table, load_timestamp, processed_by, processing_time, status) VALUES (\'billing_events\', CURRENT_TIMESTAMP(), CURRENT_USER(), 0, \'STARTED\')') %}
+              {% endif %}",
+    post_hook="{% if this.identifier != 'bz_audit_log' %}
+                {% set end_time = modules.datetime.datetime.now() %}
+                {% set duration = (end_time - start_time).total_seconds() * 1000 %}
+                {% do run_query('INSERT INTO bronze.bz_audit_log (source_table, load_timestamp, processed_by, processing_time, status) VALUES (\'billing_events\', CURRENT_TIMESTAMP(), CURRENT_USER(), ' ~ duration ~ ', \'SUCCESS\')') %}
+              {% endif %}"
+)}}
+
+-- Extract and transform billing events data from raw to bronze
+SELECT
+    -- Direct mappings from source
+    event_id,
+    user_id,
+    event_type,
+    amount,
+    event_date,
+    -- Metadata columns
+    CURRENT_TIMESTAMP() as load_timestamp,
+    CURRENT_TIMESTAMP() as update_timestamp,
+    'ZOOM_PLATFORM' as source_system
+FROM {{ source('raw', 'billing_events') }}
